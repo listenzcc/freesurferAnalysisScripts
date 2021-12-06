4053f2d0b68bb32b09d576803bf82e4fb3c356fc
listenzcc <listenzcc@mail.bnu.edu.cn>
2021-12-06 05:39:13 +0000

    Daily update

--- shells/seeAll.py
+++ shells/seeAll.py
@@ -1,22 +1,10 @@
-'''
-FileName: read_nii.py
-Author: Chuncheng
-Version: V0.0
-Purpose: An example of reading the .nii.gz file
-'''
-
 # %%
 import os
 import numpy as np
 import nibabel as nib
 
-# %%
-pwd = os.path.dirname(__file__)
-filename = os.path.join(
-    pwd, r'shells/featdir/reg_surf-lh-fsaverage/stats/zstat4.nii.gz')
 
 # %%
-
 
 def read_nifti(filename):
     img = nib.load(filename)
@@ -25,6 +13,7 @@ def read_nifti(filename):
     # The counting is as the same as the lh-fsaverage template's 163842 vertex
     # Beware that the img is of the type of nibabel.nifti1.Nifti1Image
     print('-----------------------------------------------------------')
+    print(filename)
     print(type(img), img.shape, np.prod(img.shape))
 
     # - %%
@@ -45,6 +34,24 @@ def read_nifti(filename):
 # %%
 
 
-img, hdr, data = read_nifti(filename)
+pwd = os.path.dirname(__file__)
 
+for stat in ['zstat1.nii.gz', 'zstat2.nii.gz', 'zstat3.nii.gz', 'zstat4.nii.gz', 'zstat5.nii.gz', 'zstat6.nii.gz']:
+    filename = os.path.join(
+        pwd, r'groupAnalysis/data/allsubjs-{}-lh.mgz'.format(stat))
+
+    assert os.path.isfile(filename), f'File not Found:{filename}'
+
+    img, hdr, data = read_nifti(filename)
+
+# %%
+meandata = np.mean(data, axis=-1, keepdims=True)
+print(meandata.shape)
+hdr['dims'] = meandata.shape
+img = nib.freesurfer.mghformat.MGHImage(
+    meandata.reshape((163842, 1, 1)), affine=img.affine, header=hdr, file_map=img.file_map)
+nib.save(img, os.path.join(pwd, 'example.mgz'))
+
+# %%
+np.max(meandata), np.min(meandata)
 # %%
